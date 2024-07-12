35b97c0078dac8004aa286a021a2db1ae15bf927
fedepaganelli <315604@studenti.unimore.it>
2024-07-12 17:23:29 +0200

    commit

--- src/main/java/com/carrent/controller/Controller.java
+++ src/main/java/com/carrent/controller/Controller.java
@@ -10,13 +10,36 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class Controller {
-    @FXML private TableView<Car> carTable;
-    @FXML private TableColumn<Car, String> manufacturerColumn;
-    @FXML private TableColumn<Car, String> modelColumn;
-    @FXML private TableColumn<Car, Double> dailyCostColumn;
-    @FXML private TextField manufacturerField;
-    @FXML private TextField modelField;
-    @FXML private TextField dailyCostField;
+    @FXML
+    private TableView<Car> carTable;
+    @FXML
+    private TableColumn<Car, String> manufacturerColumn;
+    @FXML
+    private TableColumn<Car, String> modelColumn;
+    @FXML
+    private TableColumn<Car, Double> dailyCostColumn;
+    @FXML
+    private TableColumn<Car, String> transmissionColumn;
+    @FXML
+    private TableColumn<Car, Integer> seatsColumn;
+    @FXML
+    private TableColumn<Car, Double> lengthColumn;
+    @FXML
+    private TableColumn<Car, Double> widthColumn;
+    @FXML
+    private TextField manufacturerField;
+    @FXML
+    private TextField modelField;
+    @FXML
+    private TextField dailyCostField;
+    @FXML
+    private TextField transmissionField;
+    @FXML
+    private TextField seatsField;
+    @FXML
+    private TextField lengthField;
+    @FXML
+    private TextField widthField;
 
     private ObservableList<Car> carList = FXCollections.observableArrayList();
 
@@ -25,6 +48,10 @@ public class Controller {
         manufacturerColumn.setCellValueFactory(new PropertyValueFactory<>("manufacturer"));
         modelColumn.setCellValueFactory(new PropertyValueFactory<>("model"));
         dailyCostColumn.setCellValueFactory(new PropertyValueFactory<>("dailyCost"));
+        transmissionColumn.setCellValueFactory(new PropertyValueFactory<>("transmission"));
+        seatsColumn.setCellValueFactory(new PropertyValueFactory<>("seats"));
+        lengthColumn.setCellValueFactory(new PropertyValueFactory<>("length"));
+        widthColumn.setCellValueFactory(new PropertyValueFactory<>("width"));
         carTable.setItems(carList);
     }
 
@@ -33,10 +60,18 @@ public class Controller {
         String make = manufacturerField.getText();
         String model = modelField.getText();
         double dailyCost = Double.parseDouble(dailyCostField.getText());
-        Car car = new Car(make, model, dailyCost);
+        String transmission = transmissionField.getText();
+        int seats = Integer.parseInt(seatsField.getText());
+        double length = Double.parseDouble(lengthField.getText());
+        double width = Double.parseDouble(widthField.getText());
+        Car car = new Car(make, model, dailyCost, transmission, seats, length, width);
         carList.add(car);
         manufacturerField.clear();
         modelField.clear();
         dailyCostField.clear();
+        transmissionField.clear();
+        seatsField.clear();
+        lengthField.clear();
+        widthField.clear();
     }
 }
--- src/main/java/com/carrent/model/Car.java
+++ src/main/java/com/carrent/model/Car.java
@@ -1,40 +1,80 @@
 package com.carrent.model;
 
 
+public class Car {
+    private String manufacturer;
+    private String model;
+    private double dailyCost;
+    private String transmission; // automatico o manuale
+    private int seats;
+    private double length;
+    private double width;
 
-    public class Car {
-        private String manufacturer;
-        private String model;
-        private double dailyCost;
 
-        public Car(String manufacturer, String model, double dailyCost) {
-            this.manufacturer= manufacturer;
-            this.model = model;
-            this.dailyCost = dailyCost;
-        }
-
-        public String getManufacturer() {
-            return manufacturer;
-        }
-
-        public void setManufacturer(String manufacturer) {
-            this.manufacturer = manufacturer;
-        }
-
-        public String getModel() {
-            return model;
-        }
-
-        public void setModel(String model) {
-            this.model = model;
-        }
-
-        public double getDailyCost() {
-            return dailyCost;
-        }
-
-        public void setDailyCost(double dailyCost) {
-            this.dailyCost = dailyCost;
-        }
+    public Car(String manufacturer, String model, double dailyCost, String transmission, int seats, double length, double width) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.dailyCost = dailyCost;
+        this.transmission = transmission;
+        this.seats = seats;
+        this.length = length;
+        this.width = width;
     }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getDailyCost() {
+        return dailyCost;
+    }
+
+    public void setDailyCost(double dailyCost) {
+        this.dailyCost = dailyCost;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+}
 
