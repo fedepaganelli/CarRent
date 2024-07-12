a3e2007dcf6ad1746d14a353bd24cfc2bfdc1819
fedepaganelli <315604@studenti.unimore.it>
2024-07-12 16:57:20 +0200

    second commit

--- src/main/java/com/carrent/model/Car.java
+++ src/main/java/com/carrent/model/Car.java
@@ -7,24 +7,34 @@ package com.carrent.model;
         private String model;
         private double dailyCost;
 
-        public Car(String make, String model, double dailyCost) {
-            this.manifacturer= make;
+        public Car(String manifacturer, String model, double dailyCost) {
+            this.manifacturer= manifacturer;
             this.model = model;
             this.dailyCost = dailyCost;
         }
 
-        public String getMake() { return manifacturer; }
+        public String getManifacturer() {
+            return manifacturer;
+        }
 
-        public void setMake(String make) { this.manifacturer = make; }
+        public void setManifacturer(String manifacturer) {
+            this.manifacturer = manifacturer;
+        }
 
-        public String getModel() { return model; }
+        public String getModel() {
+            return model;
+        }
 
-        public void setModel(String model) { this.model = model; }
+        public void setModel(String model) {
+            this.model = model;
+        }
 
-        public double getDailyCost() { return dailyCost; }
+        public double getDailyCost() {
+            return dailyCost;
+        }
 
-        public void setDailyCost(double dailyCost) { this.dailyCost = dailyCost; }
-
-
+        public void setDailyCost(double dailyCost) {
+            this.dailyCost = dailyCost;
+        }
     }
 
